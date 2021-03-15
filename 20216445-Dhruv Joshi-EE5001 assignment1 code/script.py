7d68f355ee1f2b896859a1a3b604cc5b072ec1d7
Dhruv Joshi <80222878+joshid2@users.noreply.github.com>
2021-03-15 10:01:25 +0000

    Add files via upload

---
+++ 20216445-Dhruv Joshi-EE5001 assignment1 code/script.py
@@ -0,0 +1,126 @@
+#Name: Dhruv Joshi
+#Roll number: 20216445
+#Module code:EE5001
+#  Here we start it by import the dependencies
+#Encryption and Decryption in python
+from Crypto import Random
+from Crypto.Cipher import AES
+import os
+import os.path
+from os import listdir
+from os.path import isfile, join
+import time
+
+# till here we have import all the dependencies
+
+# now we will add the class and we will also add the construtor so that we can pass the key
+#Here the key will be used to encrypt and decrypt our plain text etc.
+
+class Encryptor:
+    def __init__(self, key):
+        self.key = key
+#here we add padding so that we can balance the block size of our block
+#and at the time of decrption we will remove the padding in order to get the proper string etc.
+    def pad(self, s):
+        return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
+#now we add a function to encrypt the data 
+# here we also introduce IV(initialization vector)
+    def encrypt(self, message, key, key_size=128):
+        message = self.pad(message)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        return iv + cipher.encrypt(message)
+# now we add the encrypt file function which will take the file and then process to encrypt 
+# and in this function only the encrypted date will be shown
+    def encrypt_file(self, file_name):
+        with open(file_name, 'rb') as fo:
+            plaintext = fo.read()
+        enc = self.encrypt(plaintext, self.key)
+        with open(file_name + ".enc", 'wb') as fo:
+            fo.write(enc)
+        os.remove(file_name)
+#now we add the decrypt function which will take the cyper text and key to decrypt it
+#and here the iv function will be seperated out from the cipher text 
+    def decrypt(self, ciphertext, key):
+        iv = ciphertext[:AES.block_size]
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+        return plaintext.rstrip(b"\0")
+#added a decrypt file function 
+#this file will read all the encrypted data and will take the name in the argument and delete the encypted text and show the plain text
+
+    def decrypt_file(self, file_name):
+        with open(file_name, 'rb') as fo:
+            ciphertext = fo.read()
+        dec = self.decrypt(ciphertext, self.key)
+        with open(file_name[:-4], 'wb') as fo:
+            fo.write(dec)
+        os.remove(file_name)
+#now we crete a function get all files and help us to get all the files in the current directory or in the child of the current directory
+    def getAllFiles(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        dirs = []
+        for dirName, subdirList, fileList in os.walk(dir_path):
+            for fname in fileList:
+                if (fname != 'script.py' and fname != 'data.txt.enc'):
+                    dirs.append(dirName + "\\" + fname)
+        return dirs
+#now we create a key and object of our class,
+    def encrypt_all_files(self):
+        dirs = self.getAllFiles()
+        for file_name in dirs:
+            self.encrypt_file(file_name)
+
+    def decrypt_all_files(self):
+        dirs = self.getAllFiles()
+        for file_name in dirs:
+            self.decrypt_file(file_name)
+
+
+key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e'
+enc = Encryptor(key)
+clear = lambda: os.system('cls')
+
+if os.path.isfile('data.txt.enc'):
+    while True:
+        password = str(input("Enter password: "))
+        enc.decrypt_file("data.txt.enc")
+        p = ''
+        with open("data.txt", "r") as f:
+            p = f.readlines()
+        if p[0] == password:
+            enc.encrypt_file("data.txt")
+            break
+
+    while True:
+        clear()
+        choice = int(input(
+            "1. Press '1' to encrypt file.\n2. Press '2' to decrypt file.\n3. Press '3' to Encrypt all files in the directory.\n4. Press '4' to decrypt all files in the directory.\n5. Press '5' to exit.\n"))
+        clear()
+        if choice == 1:
+            enc.encrypt_file(str(input("Enter name of file to encrypt: ")))
+        elif choice == 2:
+            enc.decrypt_file(str(input("Enter name of file to decrypt: ")))
+        elif choice == 3:
+            enc.encrypt_all_files()
+        elif choice == 4:
+            enc.decrypt_all_files()
+        elif choice == 5:
+            exit()
+        else:
+            print("Please select a valid option!")
+
+else:
+    while True:
+        clear()
+        password = str(input("Setting up stuff. Enter a password that will be used for decryption: "))
+        repassword = str(input("Confirm password: "))
+        if password == repassword:
+            break
+        else:
+            print("Passwords Mismatched!")
+    f = open("data.txt", "w+")
+    f.write(password)
+    f.close()
+    enc.encrypt_file("data.txt")
+    print("Please restart the program to complete the setup")
